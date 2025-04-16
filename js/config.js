f86f6f8e480e1bd5efa0ff89161396da7ce9c772
伴枫 <80244085+banfeng-git@users.noreply.github.com>
2025-04-16 12:46:25 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ const MAX_HISTORY_ITEMS = 5;
 // 网站信息配置
 const SITE_CONFIG = {
     name: '知遇影视',
-    url: 'https://libretv.is-an.org',
+    url: 'https://tv.zhiyu.cloudns.be',
     description: '免费在线视频搜索与观看平台',
     logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
     version: '1.0.0'
